e857b5f0605444818329e2be32fe723a9ee0799f
Aaron Covrig <aarcov@gmail.com>
2014-03-18 18:46:57 -0400

    Still not fully functional

--- Lab.8/Tic.Tac.Toe.Game.Engine/Tic.Tac.Toe.Game.Engine/ttt_logic.cpp
+++ Lab.8/Tic.Tac.Toe.Game.Engine/Tic.Tac.Toe.Game.Engine/ttt_logic.cpp
@@ -9,9 +9,53 @@
 #include "ttt_logic.h"
 using namespace std;
 
+/* enum Player
+{
+    NOUGHT,  //  Player O's mark
+    CROSS,   //  Player X's mark
+    NONE     //  Neither player; the location has no mark
+};
+ */
+
+/*
+enum Location
+{
+    NORTHWEST,  // left-top   (northwest)
+    NORTH,      // center-top       (north)
+    NORTHEAST,  // right-top corner (northeast)
+    EAST,       // right-center     (east)
+    SOUTHEAST,  // right-bottom     (southeast)
+    SOUTH,      // center-bottom    (south)
+    SOUTHWEST,  // left-bottom      (southwest)
+    WEST,       // left-center      (west)
+    CENTER,     // center square
+    NOWHERE     // Unknown location
+};
+*/
+
+/*
+enum Status
+{
+    PLAYING,   //  No one has won and a move is available
+    WIN_NW_NE, //  Win across top row
+    WIN_W_E,   //  Win across middle row
+    WIN_SW_SE, //  Win across bottom row
+    WIN_NW_SW, //  Win along left column
+    WIN_N_S,   //  Win along center column
+    WIN_NE_SE, //  Win along right column
+    WIN_NW_SE, //  Win from left-top corner to right-bottom corner
+    WIN_NE_SW, //  Win from right-top corner to left-bottom corner
+    DRAW       //  All squares filled with no winner
+};
+ */
+
 enum Status Playing, Win_NW_NE, Win_W_E, Win_SW_SE, Win_NW_SW, Win_N_S, Win_NE_SE, Win_NW_SE, Win_NE_SW, Draw;
 
-static Player NW_player, N_player = NONE, NE_player, W_player, C_player, E_player, SW_player, S_player, SE_player;
+static Player NW_player = NONE, N_player = NONE, NE_player = NONE, W_player = NONE, C_player = NONE, E_player = NONE, SW_player = NONE, S_player = NONE, SE_player = NONE;
+
+// Setting starting player to None (blank board)
+/* Player NW_player && N_player && NE_player && W_player && C_player && E_player && SW_player && S_player && SE_player = NONE;
+ */
 
 Player now_playing = NONE;
 
