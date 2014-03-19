98e1f2116a75e85b4be189352f9af7e2f2a8d586
Aaron Covrig <aarcov@gmail.com>
2014-03-19 15:04:18 -0400

    Beta release of tic-tac-toe

--- Lab.8/Tic.Tac.Toe.Game.Engine/Tic.Tac.Toe.Game.Engine/ttt_logic.cpp
+++ Lab.8/Tic.Tac.Toe.Game.Engine/Tic.Tac.Toe.Game.Engine/ttt_logic.cpp
@@ -20,16 +20,14 @@ static Player NW_player = NONE, N_player = NONE, NE_player = NONE, W_player = NO
  */
 
 Player now_playing = CROSS;
+Player last_play;
 
 
 Status game_over = PLAYING; //set status to playing (inprogress)
 
+int round = 0;
+
 Location sq;
-
-Status check_status() {
-    return game_over;
-}
-
 
 void cycle_player(Player now_playing) {
     if (now_playing == CROSS)
@@ -38,57 +36,94 @@ void cycle_player(Player now_playing) {
         ::now_playing = CROSS;
 }
 
+Status check_status() {
+    /* if (last_play == CROSS)
+        last_play = NOUGHT;
+    else if (last_play == NOUGHT)
+        last_play = CROSS; */
+    if (NW_player == last_play && N_player == last_play && NE_player == last_play)
+        game_over = WIN_NW_NE;
+    else if (W_player == last_play && C_player == last_play && E_player == last_play)
+        game_over = WIN_W_E;
+    else if (SW_player == last_play && S_player == last_play && SE_player == last_play)
+        game_over = WIN_SW_SE;
+    else if (NW_player == last_play && W_player == last_play && SW_player == last_play)
+        game_over = WIN_NW_SW;
+    else if (N_player == last_play && C_player == last_play && S_player == last_play)
+        game_over = WIN_N_S;
+    else if (NE_player == last_play && E_player == last_play && SE_player == last_play)
+        game_over = WIN_NE_SE;
+    else if (NW_player == last_play && C_player == last_play && SE_player == last_play)
+        game_over = WIN_NW_SE;
+    else if (NE_player == last_play&& C_player == last_play && SW_player == last_play)
+        game_over = WIN_NE_SW;
+    round++;
+    if (round == 9 && game_over == PLAYING)
+        game_over = DRAW;
+    return game_over;
+    
+}
+
 bool move(Location location) {
     if (location == NORTHWEST && NW_player == NONE) {
         NW_player = current_player();
+        last_play = current_player();
         cycle_player(current_player());
         return true;
     }
     
     if (location == NORTH && N_player == NONE) {
         N_player = current_player();
+        last_play = current_player();
         cycle_player(current_player());
         return true;
     }
     
     if (location == NORTHEAST && NE_player == NONE) {
         NE_player = current_player();
+        last_play = current_player();
         cycle_player(current_player());
         return true;
     }
     
     if (location == WEST && W_player == NONE) {
         W_player = current_player();
+        last_play = current_player();
         cycle_player(current_player());
         return true;
     }
     
     if (location == CENTER && C_player == NONE) {
         C_player = current_player();
+        last_play = current_player();
         cycle_player(current_player());
         return true;
     }
     
     if (location == EAST && E_player == NONE) {
         E_player = current_player();
+        last_play = current_player();
         cycle_player(current_player());
         return true;
     }
     
     if (location == SOUTHWEST && SW_player == NONE) {
         SW_player = current_player();
+        last_play = current_player();
         cycle_player(current_player());
         return true;
     }
     
     if (location == SOUTH && S_player == NONE) {
         S_player = current_player();
+        last_play = current_player();
         cycle_player(current_player());
         return true;
     }
     
     if (location == SOUTHEAST && SE_player == NONE) {
         SE_player = current_player();
+        last_play = current_player();
         cycle_player(current_player());
         return true;
     }
@@ -101,6 +136,7 @@ bool move(Location location) {
     // else
         // return "Invalid move, this square is already owned"  Is this needed?
 }
+
 
 Player current_player() {
     // now_playing = NONE;
@@ -150,8 +186,12 @@ Player look(Location location) {
 }
 
 void clear_board() {
-    set_player(CROSS);
+    
     NW_player = N_player = NE_player = W_player = C_player = E_player = SW_player = S_player = SE_player = NONE;
+    round = 0;
+    last_play = NOUGHT;
+    set_player(CROSS);
+    game_over = PLAYING;
 }
 
     
