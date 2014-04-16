289203be2d9afe83b4f3a962888432b59040bc27
Aaron Covrig <aarcov@gmail.com>
2014-04-16 14:11:10 -0400

    Finished lab 9 (strangely, the easier prefix function was broken (one issue was comparing file vector 1 to vector 2 instead of vector 2 to vector 1 or something like that (and some other thing in prefix, may check it later)).  Adding Lab 10, just starting it.

---
+++ Lab.10/Vending Machine Object/Vending Machine Object/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Vending Machine Object
+//
+//  Created by Aaron Covrig on 4/16/14.
+//  Copyright (c) 2014 Aaron Covrig. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
